dcbf02e6a4b833989e532feaefa333b09d2b5e01
Yog9 <veryogita@gmail.com>
2020-04-20 08:51:23 +0530

    excercises added

---
+++ excercises/Exercise 2/app.js
@@ -0,0 +1,11 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: ''
+        },
+        methods:{
+            showAlert:function(){
+                return alert("Alert !!!")
+            },
+        }
+    });
